df6dd43aef2b95ad17ed73f9175f0e7a3f5d2912
Kayla Pratt <lockepratt@gmail.com>
2017-12-06 12:39:52 -0500

    Some needed stuff

---
+++ setup.bash
@@ -0,0 +1,14 @@
+#!/bin/bash          
+echo "SETUP FOR NEW PROJECT" 
+echo "------------------------------"
+echo "Directory: " 
+pwd
+
+echo "------------------------------"
+echo "NPM Install"
+npm install 
+
+echo "------------------------------"
+echo "Project NPM Pattern Lab Install"
+npm install styleguidekit-assets-default 
+npm install styleguidekit-mustache-default
---
+++ systemSetup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash          
+echo "SETUP FOR SYSTEM FOR EMORY PATTERN LAB" 
+echo "------------------------------"
+echo "Global NPM Unistalls/Install"
+npm uninstall gulp -g
+npm uninstall gulp
+npm install gulpjs/gulp-cli -g
